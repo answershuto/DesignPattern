097f0b26a6cc082d70d5d01363a3a9e2602acfac
answershuto <905307789@qq.com>
2016-11-20 21:50:29 +0800

    factory

--- src/factory.js
+++ src/factory.js
@@ -1,5 +1,11 @@
 // ES6 factory
 
+/** 
+
+ * @abstract class Animal
+
+ */
+
 class Animal {
 	constructor(){
 		if (new.target === Animal) {
@@ -15,4 +21,122 @@ class Animal {
 		throw new Error('function eat is not implemented');
 	}
 }
+
+
+
+/** 
+
+ * @class Cat
+
+ * @extends Animal
+
+ */
+
+class Cat extends Animal {
+	constructor(){
+		super();
+		console.log('I am a cat');
+	}
+
+	call(){
+		console.log('miao miao~');
+	}
+
+	eat(){
+		console.log('eat fish');
+	}
+}
+
+
+
+/** 
+
+ * @class Dog
+
+ * @extends Animal
+
+ */
+
+class Dog extends Animal {
+	constructor(){
+		super();
+		console.log('I am a dog');
+	}
+
+	call(){
+		console.log('wang wang~');
+	}
+
+	eat(){
+		console.log('eat meat');
+	}
+}
+
+
+
+/** 
+
+ * @class AnimalFactory
+
+ * factory class
+
+ * AnimalType 
+
+ */
+
+class AnimalFactory {
+	constructor(){
+		
+	}
+
+	static CreatAnimal(strAnimalType = AnimalType.Cat){
+		switch(strAnimalType) {
+			case AnimalType.Cat:
+				return new Cat();
+				break;
+			case AnimalType.Dog:
+				return new Dog();
+				break;
+		}
+	}
+}
+
+/**
+
+ * AnimalType
+
+ * @type String
+
+ */
+const AnimalType = {
+	Cat: 'Cat',
+	Dog: 'Dog'
+};
+
+
+
+
+
+(function(){
+
+	/*create a cat*/
+	let cat = AnimalFactory.CreatAnimal(AnimalType.Cat);
+	cat.call();
+	cat.eat();
+
+	/*create a dog*/
+	let dog = AnimalFactory.CreatAnimal(AnimalType.Dog);
+	dog.call();
+	dog.eat();
+
+})();
+
+
+
+
+
+
+
+
+
 
